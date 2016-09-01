7a753e73536d466661f793002ae0a3dbed985c54
Andrew Dryga <andrew@dryga.com>
2016-09-01 18:22:28 +0300

    GenericProducer module

--- lib/rbmq/producer/producer.ex
+++ lib/rbmq/producer/producer.ex
@@ -1,3 +1,75 @@
+defmodule RBMQ.GenericProducer do
+  @moduledoc """
+  AQMP channel producer.
+
+  You must configure connection (queue and exchange) before calling `publish/1` function.
+  """
+
+  @doc false
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      use GenServer
+
+      @connection Keyword.get(opts, :connection)
+      @channel_name String.to_atom("#{__MODULE__}.Channel")
+      @channel_conf Keyword.delete(opts, :connection)
+
+      unless @connection do
+        raise "You need to implement connection module and pass it in :connection option."
+      end
+
+      def start_link do
+        GenServer.start_link(__MODULE__, @channel_conf, name: __MODULE__)
+      end
+
+      def init(opts) do
+        unless Process.whereis(@connection) do
+          raise "Connection #{__MODULE__} is undefined or down."
+        end
+
+        @connection.spawn_channel(@channel_name)
+        @connection.configure_channel(@channel_name, opts)
+        chan = @connection.get_channel(@channel_name)
+
+        # TODO: reconfigure when channel is restarted
+        # Store config inside channel
+        # Process.monitor(chan.pid)
+
+        {:ok, chan}
+      end
+
+      @doc """
+      Publish new message to a linked channel.
+      """
+      def publish(data) do
+        GenServer.call(__MODULE__, {:publish, data})
+      end
+
+      @doc false
+      def handle_call({:publish, data}, _from, chan) do
+        case Poison.encode(data) do
+          {:ok, encoded_data} ->
+            _publish(chan, encoded_data)
+          {:error, _} = err ->
+            {:reply, err, chan}
+        end
+      end
+
+      defp _publish(chan, data) do
+        config = RBMQ.Connection.Channel.get_config(@channel_name)
+        is_persistent = Keyword.get(config[:queue], :durable, false)
+
+        case AMQP.Basic.publish(chan, config[:exchange][:name], config[:queue][:routing_key], data, [mandatory: true, persistent: is_persistent]) do
+          :ok ->
+            {:reply, :ok, chan}
+          _ ->
+            {:reply, :error, chan}
+        end
+      end
+    end
+  end
+end
+
 defmodule Rbmq.Producer do
   @moduledoc """
   RabbitMQ Producer
